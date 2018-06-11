45e3c3cb060260a93a7b4f73d6f89f7b52b6e10f
Ravindu Dharmasena <ravinduta@gmail.com>
2018-06-11 23:13:42 +0530

    Login Issue Fix
    Now login log out works for all components. But still it is not updated instantly.Trying to fix it

---
+++ project/src/components/payment/PaymentHandler.js
@@ -0,0 +1,11 @@
+import React,{ Component } from 'react';
+
+class PaymentHandler extends Component{
+    constructor(props){
+        super(props)
+        thi
+    }
+
+}
+
+export default PaymentHandler;
