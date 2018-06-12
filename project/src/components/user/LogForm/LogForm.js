1fc8464f8d22d5e5589135acee5c128eca3b38a8
Ravindu Dharmasena <ravinduta@gmail.com>
2018-06-12 18:40:56 +0530

    ErrorBox
    Still Constructing

--- project/src/components/user/LogForm/LogForm.js
+++ project/src/components/user/LogForm/LogForm.js
@@ -7,28 +7,38 @@ class User extends Component{
         super(props);
         this.state = {
             username:"",
-            password:""
+            password:"",
+            error:""
         }
+        this.setError = this.setError.bind(this);
+    }
+
+    setError(value){
+        this.setState({error:value});
     }
 
     setName(){
         if(this.state.username === ""){
             if(this.state.password === ""){
-                alert("Username And Password Are Empty");
-                return;
+                this.props.setError("Username And Password Are Empty");
+//                console.log(this.state.error);
+//                return;
             }
             else{
-                alert("Username Is Empty");
-                return;
+                this.props.setError("Username Is Empty");
+//                console.log(this.state.error);
+//                return;
             }
         }
         else{
             if(this.state.password === ""){
-                alert("Password Is Empty");
-                return;
+                this.props.setError("Password Is Empty");
+//                console.log(this.state.error);
+//                return;
             }
         }
 
+        if((this.state.username !== "") & (this.state.password !== ""))
         Axios.get('http://localhost:8000/user/' + this.state.username + '/' + this.state.password).then(function (data) {
             console.log(data.data);
             return data.data;
@@ -53,6 +63,14 @@ class User extends Component{
     }
 
     render(){
+        console.log(this.state.error);
+        /*let ErrorBox;
+        if(this.state.error !== ""){
+            ErrorBox = (<div className="alert alert-danger alert-dismissible">
+                <button type="button" className="close" data-dismiss="alert">&times;</button>
+                <strong>Error!</strong> {this.state.error}
+            </div>);
+        }*/
         return(
             <div>
                 <div className="card">
