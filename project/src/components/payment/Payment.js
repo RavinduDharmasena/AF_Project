b3a29d63ede03fe36ff3d6bc0b578722c0d8fe4b
hiranthaA <30893182+hiranthaA@users.noreply.github.com>
2018-06-12 18:33:08 +0530

    discharge gui new

--- project/src/components/payment/Payment.js
+++ project/src/components/payment/Payment.js
@@ -1,12 +1,64 @@
 import React,{ Component } from 'react'
-import NavigationBar from "../common/NavigationBar";
 import Footer from "../common/Footer";
+import Axios from 'axios'
 
 class Payment extends Component{
+    search(){
+        Axios.get("http://localhost:9001/getdiag").then(function (data) {
+            console.log(data.data);
+        })
+    }
+
     render(){
         return(
             <div>
-                <h1>Payment Component</h1>
+
+                <div className="card">
+                    <div className="card-header bg-info text-white">Discharge Patient</div>
+                    <div className="card-body">
+                        <div>
+                            <div>
+                                <label>Find Patient</label>
+                                <div>
+                                    <input id="patientsearch" type="text" placeholder="Enter Paitent ID Here" className="w-50 form-control"/><br/>
+                                    <input type="button" onClick={this.search.bind(this)} value="Search" className="btn btn-success"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <br/>
+
+                <div className="inline-block col-sm-5">
+                    <br/>
+                    <div className="card">
+                        <div className="card-header bg-info text-white">Patient Details</div>
+                        <div className="card-body">
+                            <div >
+                                <label>No data available</label>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="inline-block col-sm-5">
+                    <br/>
+                    <div className="card">
+                        <div className="card-header bg-info text-white">Patient Bill</div>
+                        <div className="card-body">
+                            <div>
+                                <label>No data available</label>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+
+                <br/>
+                <br/>
+                <br/>
                 <Footer/>
             </div>
         );
---
+++ project/src/components/payment/patientdetails.js
@@ -0,0 +1,14 @@
+import React,{ Component } from 'react'
+
+class Comp extends Component{
+
+    render(){
+        return(
+            <input type="text"/>
+        );
+
+    }
+
+}
+
+export default Comp;
