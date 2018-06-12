e85e1c10b712e1d16d99ce98f3249537652e01c7
Ravindu Dharmasena <ravinduta@gmail.com>
2018-06-12 10:23:09 +0530

    Added a dependency
    Added Axios dependency for database transactions

--- project/src/components/user/Handler/UserHandler.js
+++ project/src/components/user/Handler/UserHandler.js
@@ -6,16 +6,44 @@ import Footer from "../../common/Footer";
 import "./UserHandler.css";
 import UserInfo from "../UserInfo/UserInfo";
 import UserProfile from "../../../closure/UserProfile";
+import Payment from "../../payment/Payment";
 
 class UserHandler extends Component{
-    render(){
+    constructor(props){
+        super(props);
+        this.state = {
+            logged:false,
+            run:"user"
+        }
+        this.setLogged = this.setLogged.bind(this);
+        this.setRun = this.setRun.bind(this);
+    }
+
+    setLogged(value){
+        this.setState({logged:value});
+    }
+
+    setRun(value){
+        this.setState({run:value});
+    }
+
+    componentWillMount(){
         if(UserProfile.getName() === ""){
+            this.setLogged(false);
+        }
+        else{
+            this.setLogged(true);
+        }
+    }
+
+    render(){
+        if(!this.state.logged){//(UserProfile.getName() === ""){
             return(
                 <div>
-                    <NavigationBar/>
+                    <NavigationBar setRun={this.setRun}/>
                     <div className="inline-block col-sm-5">
                         <br/>
-                        <LogForm/>
+                        <LogForm setLogged={this.setLogged}/>
                     </div>
                     <div className="inline-block col-sm-7">
                         <br/>
@@ -26,22 +54,31 @@ class UserHandler extends Component{
             )
         }
         else{
-            return(
-                <div>
+            if(this.state.run === "user"){
+                return(
                     <div>
-                        <NavigationBar/>
-                        <div className="inline-block col-sm-5">
-                            <br/>
-                            <UserInfo/>
+                        <div>
+                            <NavigationBar setLogged={this.setLogged} setRun={this.setRun}/>
+                            <div className="inline-block col-sm-5">
+                                <br/>
+                                <UserInfo/>
+                            </div>
+                            <div className="inline-block col-sm-7">
+                                <br/>
+                                <Overview/>
+                            </div>
+                            <Footer/>
                         </div>
-                        <div className="inline-block col-sm-7">
-                            <br/>
-                            <Overview/>
-                        </div>
-                        <Footer/>
                     </div>
+                );
+            }
+            else if(this.state.run === "payment"){
+                <div>
+                    <NavigationBar/>
+                    <Payment/>
+                    <Footer/>
                 </div>
-            );
+            }
         }
     }
 }
