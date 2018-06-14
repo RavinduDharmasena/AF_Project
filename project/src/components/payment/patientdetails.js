34384dbe86ea8c71c6e1aaf15a0b35c17c6a0c96
hiranthaA <30893182+hiranthaA@users.noreply.github.com>
2018-06-14 09:14:46 +0530

    Bill generation completed

--- project/src/components/payment/patientdetails.js
+++ project/src/components/payment/patientdetails.js
@@ -2,9 +2,64 @@ import React,{ Component } from 'react'
 
 class PatientDetails extends Component{
 
+    constructor(props){
+        super(props);
+
+        this.state = {
+            patientdata : this.props.patientdata
+        }
+    }
+
+    componentDidMount(){
+        console.log("this is state from prop details"+this.props.patientdata);
+    }
+
+
     render(){
+        let pdetails = this.state.patientdata;
         return(
-            <input type="text"/>
+
+            <table className="table table-bordered table-hover">
+                <tbody>
+                    <tr>
+                        <th>ID</th>
+                        <td>{pdetails._id}</td>
+                    </tr>
+                    <tr>
+                        <th>Full Name</th>
+                        <td>{pdetails.fname+" "+pdetails.lname}</td>
+                    </tr>
+                    <tr>
+                        <th>Age</th>
+                        <td>{pdetails.age}</td>
+                    </tr>
+                    <tr>
+                        <th>Gender</th>
+                        <td>{pdetails.gender}</td>
+                    </tr>
+                    <tr>
+                        <th>NIC</th>
+                        <td>{pdetails.nic}</td>
+                    </tr>
+                    <tr>
+                        <th>Admitted Date</th>
+                        <td>{pdetails.admitted_date}</td>
+                    </tr>
+                    <tr>
+                        <th>Ward</th>
+                        <td>{pdetails.ward}</td>
+                    </tr>
+                    <tr>
+                        <th>Custodian Name</th>
+                        <td>{pdetails.custodian_name}</td>
+                    </tr>
+                    <tr>
+                        <th>Custodian Contact</th>
+                        <td>{pdetails.custodian_contact}</td>
+                    </tr>
+                </tbody>
+            </table>
+
         );
     }
 
