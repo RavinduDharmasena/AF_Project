e2288790a3aae4766bc37e3c5e2d244c83b45c02
Kayathiri <thillaikayatk@gmail.com>
2018-06-15 11:25:19 +0530

    added css

--- project/src/components/diagnosis/Diagosis.js
+++ project/src/components/diagnosis/Diagosis.js
@@ -7,135 +7,125 @@ class Diagosis extends Component{
     render(){
         return(
             <div>
-            <div className="card">
-            <div className="card-header bg-info text-white">Admit Patient</div>
-        <div className="card-body">
-            <div>
-            <div>
-            <div>
-            <label>NIC:</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <div className="w-50 inline-block" >
-            <input id="patientnic" onChange={this.setSearchPID.bind(this)} type="text" placeholder="Enter Paitent NIC Here" className=" form-control"/>
-            </div>
-            </div>
-            <label>Patient Id:</label>&nbsp;
-        <div className="w-50 inline-block" >
-            <input id="patientid" onChange={this.setSearchPID.bind(this)} type="text" placeholder="Enter Paitent ID Here" className=" form-control"/>
-            </div>
-            </div>
-            </div>
-            </div>
-            </div>
 
-            <br/>
+                <div>
+                    <div className="cont">
+                        <p className="text">
+                            <label htmlFor="usr">NIC: </label>
+                        </p>
+                        <input type="text" className="form-control" id="usr"/>
+                    </div>
+                    <div className="cont">
+                        <p className="text">
+                            <label htmlFor="usr">Patient ID: </label>
+                        </p>
+                        <input type="text" className="form-control" id="usr"/>
+                    </div>
+                </div>
 
-            <div className="inline-block col-sm-6">
-            <br/>
-            <div className="card">
-            <div className="card-header bg-info text-white">Complaints:</div>
-        <div className="card-body">
-            <div className="w-50 inline-block" >
-            <textarea id="complaints" onChange={this.setSearchPID.bind(this)} type="text" className=" form-control"></textarea>
-            </div>
-            </div>
-            </div>
-            </div>
-            <div className="inline-block col-sm-6">
-            <br/>
-            <div className="card">
-            <div className="card-header bg-info text-white">Allergies</div>
-            <div className="card-body">
-            <div className="w-50 inline-block" >
-            <textarea id="allergies" onChange={this.setSearchPID.bind(this)} type="text"  className=" form-control"></textarea>
-            </div>
-            </div>
-            </div>
-            </div>
-            <div className="inline-block col-sm-6">
-            <br/>
-            <div className="card">
-            <div className="card-header bg-info text-white">Physical Examination</div>
-        <div className="card-body">
-            <div className="w-50 inline-block" >
-            <textarea id="physical" onChange={this.setSearchPID.bind(this)} type="text"  className=" form-control"></textarea>
-            </div>
-            </div>
-            </div>
-            </div>
-            <br/>
-            <br/>
-            <br/>
-            <div className="card">
-            <div className="card-header bg-info text-white">Treatement:</div>
-        <div className="card-body">
-            <div>
-            <div className="inline-block col-sm-4" >
-            <input type="checkbox"/> Injection &nbsp;&nbsp;
-    <input id="injectadd" type="button" onClick={this.search.bind(this)} value="Add" className="btn btn-success"/>
-            </div>
-            <div className="inline-block col-sm-4" >
-            <input type="checkbox"/> Sewing &nbsp;&nbsp;
-    <input id="injectadd" type="button" onClick={this.search.bind(this)} value="Add" className="btn btn-success"/>
-            </div>
-            <br/>
-            <br/>
-            <div className="inline-block col-sm-1" >
-            <div class="dropdown">
-            <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Inject Id
-        <span class="caret"></span></button>
-        <ul class="dropdown-menu">
-            <li><a href="#">HTML</a></li>
-        <li><a href="#">CSS</a></li>
-        <li><a href="#">JavaScript</a></li>
-        </ul>
-        </div>
-        </div>
-        <div className="inline-block col-sm-3" >
-            <div>
-            <label>Quantity:</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <div className="w-50 inline-block" >
-            <input id="qty" onChange={this.setSearchPID.bind(this)} type="text"  className=" form-control"/>
-            </div>
-            </div>
-            </div>
-            </div>
-            </div>
-            </div>
+                <div>
+                    <div className="cont">
+                        <p className="text">Complaints</p>
+                        <form>
+                            <div>
+                                <textarea className="form-control" rows="5" id="comment"></textarea>
+                            </div>
+                        </form>
+                    </div>
+                </div>
 
-            <div className="card">
-            <div className="card-header bg-info text-white">Prescription:</div>
-        <div className="card-body">
-            <div className="inline-block col-sm-1" >
-            <div class="dropdown">
-            <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Drugs
-            <span class="caret"></span></button>
-        <ul class="dropdown-menu">
-            <li><a href="#">HTML</a></li>
-        <li><a href="#">CSS</a></li>
-        <li><a href="#">JavaScript</a></li>
-        </ul>
-        </div>
-        </div>
-        <div className="inline-block col-sm-3" >
-            <div>
-            <label>Quantity:</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <div className="w-50 inline-block" >
-            <input id="qty" onChange={this.setSearchPID.bind(this)} type="text"  className=" form-control"/>
-            </div>
-            </div>
-            </div>
-            </div>
-            </div>
-            <div>
-            <input id="view" type="button" onClick={this.search.bind(this)} value="View" className="btn btn-success"/>&nbsp;&nbsp;
-    <input id="update" type="button" onClick={this.search.bind(this)} value="Update" className="btn btn-success"/>&nbsp;&nbsp;&nbsp;
-    <input id="ok" type="button" onClick={this.search.bind(this)} value="Ok" className="btn btn-success"/>
+                <div>
+                    <div className="cont">
+                        <p className="text">Allergies</p>
+                        <form>
+                            <div>
+                                <textarea className="form-control" rows="5" id="comment"></textarea>
+                            </div>
+                        </form>
+                    </div>
+                </div>
 
-            </div>
-            <br/>
-            <br/>
-            <br/>
-            <Footer/>
+                <div>
+                    <div className="cont">
+                        <p className="text">Physical Examination</p>
+                        <form>
+                            <div>
+                                <textarea className="form-control" rows="5" id="comment"></textarea>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="cont1">
+                        <p className="text">Treatement: </p>
+                        <form>
+                            <div>
+                                <label className="text">
+                                    <input name="inject" type="checkbox"/> Injection
+                                    &nbsp;&nbsp;
+                                    <button type="button" className="btn btn-primary btn-sm"> add</button>
+                                </label>
+                                <label className="text">
+                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input
+                                    name="inject" type="checkbox"/> Sewing
+                                    &nbsp;&nbsp;
+                                    <button type="button" className="btn btn-primary btn-sm"> add</button>
+                                </label>
+                            </div>
+                        </form>
+                        <div>
+                            <button className="btn btn-primary dropdown-toggle" type="button"
+                                    data-toggle="dropdown">InjectID
+                                <span className="caret"></span></button>
+                            <ul className="dropdown-menu">
+                                <li><a href="#">HTML</a></li>
+                                <li><a href="#">CSS</a></li>
+                                <li><a href="#">JavaScript</a></li>
+                            </ul>
+                        </div>
+
+                        <div>
+                            <p className="text">
+                                <label htmlFor="usr">quantity </label>
+                            </p>
+                            <input type="text" className="form-control" id="usr"/>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div>
+                    <div className="cont2">
+                        <p className="text">Prescription: </p>
+                        <form>
+                            <div>
+                                <button className="btn btn-primary dropdown-toggle" type="button"
+                                        data-toggle="dropdown">Drugs
+                                    <span className="caret"></span></button>
+                                <ul className="dropdown-menu">
+                                    <li><a href="#">HTML</a></li>
+                                    <li><a href="#">CSS</a></li>
+                                    <li><a href="#">JavaScript</a></li>
+                                </ul>
+                            </div>
+                            <div>
+                                <p className="text">
+                                    <label htmlFor="usr">quantity </label>
+                                </p>
+                                <input type="text" className="form-control" id="usr"/>
+                            </div>
+                            <br/>
+                            <button type="button" className="btn btn-primary btn-sm"> View</button>
+                            &nbsp;&nbsp;
+                            <button type="button" className="btn btn-primary btn-sm"> Update</button>
+                            &nbsp;&nbsp;
+                            <button type="button" className="btn btn-primary btn-sm"> OK</button>
+
+                        </form>
+                    </div>
+                </div>
+
             </div>
         );
     }
